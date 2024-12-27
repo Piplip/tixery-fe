6b55221190cd4700f8427f9cd096b4b44132d6cc
Piplip <93427122+Piplip@users.noreply.github.com>
2024-12-28 00:33:33 +0700

    Adding User Data Form

--- src/component/UserCollectDataTemplate.jsx
+++ src/component/UserCollectDataTemplate.jsx
@@ -1,8 +1,15 @@
 import {Outlet} from "react-router-dom";
+import '../styles/user-collect-data-template-styles.css'
 
 function UserCollectDataTemplate(){
     return (
-        <Outlet />
+        <div className={'user-collect-data-template-container'}>
+            {/* TODO: should replace a dedicated navigation for user collect data instead of default navigation */}
+            {/*<TopNav />*/}
+            <div className={'user-collect-data-outlet-wrapper'}>
+                <Outlet />
+            </div>
+        </div>
     )
 }
 
