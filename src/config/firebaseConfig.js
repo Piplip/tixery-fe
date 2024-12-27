4b3f8d0154dda54fa1c2011ffe08373d23018a1b
Piplip <93427122+Piplip@users.noreply.github.com>
2024-12-28 00:32:00 +0700

    adding Firebase config

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD-0rgPyCLJgTGDGOrVoWJnRl5MyJQH-ms",
+    authDomain: "medicare-10c3b.firebaseapp.com",
+    projectId: "medicare-10c3b",
+    storageBucket: "medicare-10c3b.appspot.com",
+    messagingSenderId: "1060080079680",
+    appId: "1:1060080079680:web:d16f9fbf1615f3b240e0a5",
+    measurementId: "G-KDPH5ESQFM"
+};
