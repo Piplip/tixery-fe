487ba223d10cedc7e9c5562b02b1d42577d8b460
Piplip <93427122+Piplip@users.noreply.github.com>
2024-12-28 00:32:31 +0700

    adding navigation UI for logged user

---
+++ src/component/LoggedInUserNav.jsx
@@ -0,0 +1,67 @@
+import "../styles/logged-in-user-nav-styles.css"
+import {getDownloadURL, getStorage, ref} from "firebase/storage";
+import {initializeApp} from "firebase/app";
+import {firebaseConfig} from "../config/firebaseConfig.js";
+import {Avatar, Stack} from "@mui/material";
+import {getUserData, logout} from "../common/Utilities.js";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+
+function LoggedInUserNav(){
+    initializeApp(firebaseConfig);
+    const storage = getStorage()
+    const [ppImage, setPpImage] = useState(null)
+    const attendeeOptions = [
+        { name: 'Browse Events', link: '/events' },
+        { name: 'Manage my events', link: '/manage-events' },
+        { name: 'Tickets', link: '/tickets' },
+        { name: 'Likes', link: '/likes' },
+        { name: 'Followings', link: '/followings' },
+        { name: 'Interests', link: '/interests' },
+        { name: 'Account Settings', link: '/account-settings' },
+        { name: 'Profile', link: '/profile' },
+    ];
+
+    useEffect(() => {
+        let url = getUserData('profileImageUrl')
+        if(url === null) return
+        let storageRef = ref(storage, url)
+        getDownloadURL(storageRef)
+            .then(url => {
+                console.log(url)
+                setPpImage(url)
+            })
+            .catch(() => {
+                console.log('error')
+            })
+    }, []);
+
+    return (
+        <div className={'logged-in-user-nav'}>
+            <Stack direction={'row'} alignItems={'center'} columnGap={1}>
+                <Avatar src={ppImage} alt="profile">
+                    {getUserData('fullName').charAt(0)}
+                </Avatar>
+                <p>{getUserData('fullName')}</p>
+                <KeyboardArrowDownIcon />
+            </Stack>
+            <Stack className={'user-sub-nav'} rowGap={1}>
+                {attendeeOptions.map((item, index) => {
+                    return (
+                        <Link to={item.link} key={index}>
+                            <div>
+                                <p>{item.name}</p>
+                            </div>
+                        </Link>
+                    )
+                })}
+                <div onClick={logout}>
+                    <p>Logout</p>
+                </div>
+            </Stack>
+        </div>
+    )
+}
+
+export default LoggedInUserNav
