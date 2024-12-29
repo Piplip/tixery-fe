7aaa443a94a2e728ca2c296762f04e4d19fe6435
Piplip <93427122+Piplip@users.noreply.github.com>
2024-12-29 22:24:46 +0700

    remove unnecessary code

--- src/component/UserCollectDataTemplate.jsx
+++ src/component/UserCollectDataTemplate.jsx
@@ -4,8 +4,6 @@ import '../styles/user-collect-data-template-styles.css'
 function UserCollectDataTemplate(){
     return (
         <div className={'user-collect-data-template-container'}>
-            {/* TODO: should replace a dedicated navigation for user collect data instead of default navigation */}
-            {/*<TopNav />*/}
             <div className={'user-collect-data-outlet-wrapper'}>
                 <Outlet />
             </div>
