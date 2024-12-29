ff46feb7f857eb8113715e14cd8e85cbb269dd27
Piplip <93427122+Piplip@users.noreply.github.com>
2024-12-29 22:23:13 +0700

    adjust role name

--- src/component/LoggedInUserNav.jsx
+++ src/component/LoggedInUserNav.jsx
@@ -3,7 +3,7 @@ import {getDownloadURL, getStorage, ref} from "firebase/storage";
 import {initializeApp} from "firebase/app";
 import {firebaseConfig} from "../config/firebaseConfig.js";
 import {Avatar, Stack} from "@mui/material";
-import {checkLoggedIn, getUserData, logout} from "../common/Utilities.js";
+import {checkLoggedIn, getUserData, hasRole, logout} from "../common/Utilities.js";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
@@ -13,21 +13,21 @@ function LoggedInUserNav(){
     const storage = getStorage()
     const [ppImage, setPpImage] = useState(null)
     const attendeeOptions = [
-        { name: 'Browse Events', link: '/events' },
-        { name: 'Manage my events', link: '/manage-events' },
-        { name: 'Tickets', link: '/tickets' },
-        { name: 'Likes', link: '/likes' },
-        { name: 'Followings', link: '/followings' },
-        { name: 'Interests', link: '/interests' },
-        { name: 'Account Settings', link: '/account-settings' },
-        { name: 'Profile', link: '/profile' },
+        { name: 'Browse Events', link: '/events', roles: ['attendee'] },
+        { name: 'Manage my events', link: '/manage-events', roles: ['host'] },
+        { name: 'Tickets', link: '/tickets', roles: ['attendee'] },
+        { name: 'Likes', link: '/likes' , roles: ['attendee']},
+        { name: 'Followings', link: '/followings', roles: ['attendee'] },
+        { name: 'Interests', link: '/interests', roles: ['attendee'] },
+        { name: 'Account Settings', link: '/account-settings', public: true},
+        { name: 'Profile', link: '/profile', public: true},
     ];
     const [fullName, setFullName] = useState(getUserData('fullName'))
 
     useEffect(() => {
         if(checkLoggedIn()){
             let url = getUserData('profileImageUrl')
-            if(url === null) return
+            if(url === null || url === "") return
             if(url.includes('https://lh3.googleusercontent.com')){
                 setPpImage(url)
                 return
@@ -45,24 +45,26 @@ function LoggedInUserNav(){
 
     return (
         <div className={'logged-in-user-nav'}>
-            {fullName &&
-                <Stack direction={'row'} alignItems={'center'} columnGap={1}>
-                    <Avatar src={ppImage} alt="profile">
-                        {fullName.charAt(0)}
-                    </Avatar>
-                    <p>{fullName}</p>
-                    <KeyboardArrowDownIcon />
-                </Stack>
-            }
+
+            <Stack direction={'row'} alignItems={'center'} columnGap={1}>
+                <Avatar src={ppImage} alt="profile">
+                    {fullName.charAt(0)}
+                </Avatar>
+                {fullName && <p>{fullName}</p>}
+                <KeyboardArrowDownIcon />
+            </Stack>
             <Stack className={'user-sub-nav'} rowGap={1}>
                 {attendeeOptions.map((item, index) => {
-                    return (
-                        <Link to={item.link} key={index}>
-                            <div>
-                                <p>{item.name}</p>
-                            </div>
-                        </Link>
-                    )
+                    const cond = item.roles instanceof Array ? hasRole(item.roles) : []
+                    if(cond || item.public){
+                        return (
+                            <Link to={item.link} key={index}>
+                                <div>
+                                    <p>{item.name}</p>
+                                </div>
+                            </Link>
+                        )
+                    }
                 })}
                 <div onClick={logout}>
                     <p>Logout</p>
