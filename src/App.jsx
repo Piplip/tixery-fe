40a2af56ae1850f0eb7c280e4438e6619d549c09
Piplip <93427122+Piplip@users.noreply.github.com>
2025-01-02 22:22:35 +0700

    adding loaders for organizer routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,20 @@ import UserCollectDataTemplate from "./component/UserCollectDataTemplate.jsx";
 import AttendeeCollectnfo from "./component/attendee/AttendeeCollectnfo.jsx";
 import {LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
-import OrganizerCollectInfo from "./component/OrganizerCollectInfo.jsx";
+import OrganizerCollectInfo from "./component/organizer/OrganizerCollectInfo.jsx";
 import OrganizerOverview from "./component/organizer/OrganizerOverview.jsx";
 import OrganizerTemplate from "./component/organizer/OrganizerTemplate.jsx";
 import OrganizerHome from "./component/organizer/OrganizerHome.jsx";
 import OrganizerSettings from "./component/organizer/OrganizerSettings.jsx";
 import OrganizerSettingProfile from "./component/organizer/OrganizerSettingProfile.jsx";
+import OrganizerViewTemplate from "./component/organizer/OrganizerViewTemplate.jsx";
+import OrganizerView from "./component/organizer/OrganizerView.jsx";
+import {accountAxiosWithToken} from "./config/axiosConfig.js";
+import {getUserData} from "./common/Utilities.js";
+import {Suspense} from "react";
+import LoadingFallback from "./component/LoadingFallback.jsx";
+import OrganizerNewProfile from "./component/organizer/OrganizerNewProfile.jsx";
+import OrganizerEditProfile from "./component/organizer/OrganizerEditProfile.jsx";
 
 function App() {
     const routers = createBrowserRouter([
@@ -41,22 +49,60 @@ function App() {
             path: '/organizer',
             element: <OrganizerTemplate />,
             children: [
-                {index: true, element: <OrganizerHome />},
+                {
+                    index: true,
+                    element: <OrganizerHome />,
+                },
                 {
                     path: 'u',
                     element: <OrganizerSettings />,
                     children: [
-                        {index: true, element: <OrganizerSettingProfile />}
+                        {index: true, element: <OrganizerSettingProfile />,
+                            loader: () => accountAxiosWithToken.get('/organizer/profile?u=' + getUserData("sub"))
+                        }
                     ]
+                },
+                {path: 'profile/info', element: <OrganizerNewProfile />},
+                {
+                    path: 'profile/info/:id', element: <OrganizerEditProfile />,
+                    loader: async ({ params }) => {
+                        const profileId = params.id;
+
+                        const response = await accountAxiosWithToken.get(
+                            `/organizer/profile/get?pid=${profileId}`
+                        );
+
+                        return response.data;
+                    },
+                }
+            ]
+        },
+        {
+            path: '/o',
+            element: <OrganizerViewTemplate />,
+            children: [
+                {
+                    path: ':name', element: <OrganizerView />,
+                    loader: async ({ params }) => {
+                        const organizerName = params.name;
+
+                        const response = await accountAxiosWithToken.get(
+                            `/organizer/profile/get?pid=${organizerName}`
+                        )
+
+                        return response.data;
+                    },
                 }
             ]
         }
     ])
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <RouterProvider router={routers} />
-        </LocalizationProvider>
+        <Suspense fallback={<LoadingFallback />}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <RouterProvider router={routers} />
+            </LocalizationProvider>
+        </Suspense>
     )
 }
 
