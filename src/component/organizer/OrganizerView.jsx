9ab6204ff4f45b4e72d846e396f00c39fe03651b
Piplip <93427122+Piplip@users.noreply.github.com>
2025-01-04 00:07:16 +0700

    adjust UIs

--- src/component/organizer/OrganizerView.jsx
+++ src/component/organizer/OrganizerView.jsx
@@ -1,18 +1,19 @@
 import "../../styles/organizer-view-styles.css"
-import {Avatar, IconButton, Stack} from "@mui/material";
-import LanguageIcon from '@mui/icons-material/Language';
-import {useLoaderData} from "react-router-dom";
+import {Avatar, Stack} from "@mui/material";
+import {Link, useLoaderData} from "react-router-dom";
 import {initializeApp} from "firebase/app";
 import {firebaseConfig} from "../../config/firebaseConfig.js";
 import {getDownloadURL, getStorage, ref} from "firebase/storage";
 import {useCallback, useEffect, useState} from "react";
+import LoadingFallback from "../LoadingFallback.jsx";
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import XIcon from '@mui/icons-material/X';
 
 function OrganizerView() {
     initializeApp(firebaseConfig);
     const storage = getStorage()
     const data = useLoaderData()
     const [profileImage, setProfileImage] = useState(null)
-    console.log(data)
 
     const loadImage = useCallback(async (url) => {
         if (!url) return null;
@@ -24,52 +25,64 @@ function OrganizerView() {
             return null;
         }
     }, [storage]);
-
+    console.log(data)
     useEffect(() => {
-        if(data){
+        if(data['profile_image_url']){
             loadImage(data['profile_image_url']).then((url) => setProfileImage(url))
+        }
+        else {
+            setProfileImage("#")
         }
     }, [])
 
-    return (
-        <div className={'organizer-view-wrapper'}>
-            {data &&
-                <div className={'organizer-view-hero'}>
-                    <div className={'organizer-view-hero__img-wrapper'}>
-                        <img className={'organizer-view-hero__img'}
-                             src={profileImage || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZdM4Naw6g6mBZtWbsk8r4u0DEauhmvKrpFg&s'}
-                             alt={'hero image'}/>
-                    </div>
-                    <Stack className={'organizer-view-hero__main'} alignItems={'center'} rowGap={3}>
-                        <Stack alignItems={'center'} rowGap={2}>
-                            <Avatar sx={{width: '10rem', height: '10rem', fontSize: '5rem'}}
-                                src={profileImage}
-                                    alt={'profile'}>
-                                {data['profile_name'].split(' ')[0][0]}
-                            </Avatar>
-                            <p className={'organizer-view-hero__profile-title'}>
-                                {data['profile_name']}
-                            </p>
-                        </Stack>
-                        <Stack direction={'row'} columnGap={1}>
-                            <button>Follow</button>
-                            <button>Contact</button>
-                        </Stack>
-                        <Stack rowGap={1}>
-                            <p className={'organizer-view-hero__profile-description'}>
-                                {data['description']}
-                            </p>
-                            <IconButton className={'organizer-view-hero__social-btn'}>
-                                <LanguageIcon fontSize="inherit"/>
-                            </IconButton>
-                        </Stack>
-                    </Stack>
-                </div>
-            }
-            <div className={'organizer-view__main'}>
 
+    return (
+        profileImage ?
+            <div className={'organizer-view-wrapper'}>
+                {data &&
+                    <div className={'organizer-view-hero'}>
+                        <div className={'organizer-view-hero__img-wrapper'}>
+                            <img className={'organizer-view-hero__img'}
+                                 src={(profileImage || profileImage !== "#") || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZdM4Naw6g6mBZtWbsk8r4u0DEauhmvKrpFg&s'}
+                                 alt={'hero image'}/>
+                        </div>
+                        <Stack className={'organizer-view-hero__main'} alignItems={'center'} rowGap={3}>
+                            <Stack alignItems={'center'} rowGap={2}>
+                                <Avatar sx={{width: '10rem', height: '10rem', fontSize: '5rem'}}
+                                        src={profileImage}
+                                        alt={'profile'}>
+                                    {data['profile_name'].split(' ')[0][0]}
+                                </Avatar>
+                                <p className={'organizer-view-hero__profile-title'}>
+                                    {data['profile_name']}
+                                </p>
+                            </Stack>
+                            <Stack direction={'row'} columnGap={1}>
+                                <button>Follow</button>
+                                <button>Contact</button>
+                            </Stack>
+                            <Stack rowGap={1}>
+                                <p className={'organizer-view-hero__profile-description'}>
+                                    {data['description']}
+                                </p>
+                                <Stack direction={'row'} alignSelf={'center'} flexGrow={1} marginTop={5} columnGap={2}>
+                                    {data['social_media_links'].split(',').map((link, index) => {
+                                        return (
+                                            <Link to={link} key={index} target={'_blank'}>
+                                                {link.includes('facebook') ?
+                                                    <FacebookRoundedIcon sx={{color: 'blue'}}/> : <XIcon />
+                                                }
+                                            </Link>
+                                        )
+                                    })}
+                                </Stack>
+                            </Stack>
+                        </Stack>
+                    </div>
+                }
             </div>
-        </div>
+            :
+            <LoadingFallback />
     )
 }
 
