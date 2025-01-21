0e9b7e0a93fc91f26f349685b1c67c8156ec8bad
Piplip <93427122+Piplip@users.noreply.github.com>
2025-01-21 21:33:05 +0700

    adding transform number utilities function

--- src/common/Utilities.js
+++ src/common/Utilities.js
@@ -64,3 +64,13 @@ export async function fetchImage(storage, imagePath) {
         throw error;
     }
 }
+
+export function transformNumber(number){
+    if(number > 1000000){
+        return (number / 1000000).toFixed(1) + 'M';
+    }
+    if(number > 1000){
+        return (number / 1000).toFixed(1) + 'K';
+    }
+    return number;
+}
