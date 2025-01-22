0ec96413d000d467bcf8662bb3f2f4c2ed96b50c
Piplip <93427122+Piplip@users.noreply.github.com>
2025-01-22 20:26:31 +0700

    Adding share and like UI to event card Fixing UI logic on clicking profile name Adding fallback image

--- src/component/shared/EventCard.jsx
+++ src/component/shared/EventCard.jsx
@@ -7,6 +7,8 @@ import {firebaseConfig} from "../../config/firebaseConfig.js";
 import {getStorage, ref, getDownloadURL} from "firebase/storage";
 import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShareDialog from "./ShareDialog.jsx";
 
 EventCard.propTypes = {
     event: PropTypes.shape({
@@ -41,14 +43,22 @@ function EventCard ({ event, organizer, id, customURL}) {
 
     return (
         <Stack className="event-card" onClick={() => window.location.href = `/events/${event.event_id}`}>
-            <img className="event-card__image"
-                 src={imageUrl || "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F936315053%2F558993483103%2F1%2Foriginal.20250115-135317?crop=focalpoint&fit=crop&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=3a03308f50db1e157ca93403975dcc59"}
-                 alt={"foo"}/>
+            <Stack style={{position: 'relative'}}>
+                <img className="event-card__image"
+                     src={imageUrl || "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F936315053%2F558993483103%2F1%2Foriginal.20250115-135317?crop=focalpoint&fit=crop&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=3a03308f50db1e157ca93403975dcc59"}
+                     alt={"foo"}/>
+                <Stack className={'event-card__action'} direction={'row'} onClick={e => e.stopPropagation()}>
+                    <FavoriteBorderIcon />
+                    <ShareDialog eventID={event.event_id}/>
+                </Stack>
+            </Stack>
             <Stack rowGap={.5} padding={'0 1rem 1rem 1rem'} marginTop={1}>
                 <p className="event-card__title">{event.name}</p>
                 <p className="event-card__date">{dayjs(event.start_time).format("ddd, DD MMM")} • {dayjs(event.start_time).format("HH:mm [GMT]Z")}</p>
                 <p className="event-card__price">{event.price === 'Free' ? 'Free ' : `From $${event.price}`}</p>
-                <Link to={`/organizer/${customURL || id}`} className="event-card__organizer">
+                <Link to={`/o/${customURL || id}`} className="event-card__organizer" target={'_blank'}
+                    onClick={(e) => e.stopPropagation()}
+                >
                     <p className="event-card__organizer">{organizer}</p>
                 </Link>
             </Stack>
