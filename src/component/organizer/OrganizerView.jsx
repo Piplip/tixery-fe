0ae7a9898c0aa35153dd5fd0142ba9cba1311f44
Piplip <93427122+Piplip@users.noreply.github.com>
2025-01-22 20:29:15 +0700

    adding load profile events

--- src/component/organizer/OrganizerView.jsx
+++ src/component/organizer/OrganizerView.jsx
@@ -1,24 +1,37 @@
 import "../../styles/organizer-view-styles.css"
-import {Avatar, Stack} from "@mui/material";
+import {Avatar, Stack, Typography} from "@mui/material";
 import {Link, useLoaderData} from "react-router-dom";
 import {initializeApp} from "firebase/app";
 import {firebaseConfig} from "../../config/firebaseConfig.js";
 import {getDownloadURL, getStorage, ref} from "firebase/storage";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import LoadingFallback from "../shared/LoadingFallback.jsx";
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import XIcon from '@mui/icons-material/X';
 import LanguageIcon from '@mui/icons-material/Language';
+import {transformNumber} from "../../common/Utilities.js";
+import {eventAxios} from "../../config/axiosConfig.js";
+import dayjs from "dayjs";
+import EventCard from "../shared/EventCard.jsx";
+import Grid from '@mui/material/Grid2';
+
+initializeApp(firebaseConfig);
+const storage = getStorage()
 
 function OrganizerView() {
-    initializeApp(firebaseConfig);
-    const storage = getStorage()
-    const data = useLoaderData()
-    const [profileImage, setProfileImage] = useState(null)
+    const data = useLoaderData();
+    const [profileImage, setProfileImage] = useState(null);
+    const [profileEvents, setProfileEvents] = useState(null);
+    const [activeTab, setActiveTab] = useState(0);
+    const hasFetchedProfileEvents = useRef(false);
+
+    const totalUpcoming = profileEvents?.reduce((acc, event) => {
+        return dayjs(event.start_time).isAfter(dayjs()) ? acc + 1 : 0
+    })
 
     const loadImage = useCallback(async (url) => {
         if (!url) return null;
-        if(url.includes('googleusercontent')) return url;
+        if (url.includes('googleusercontent')) return url;
         try {
             const storageRef = ref(storage, url);
             return await getDownloadURL(storageRef);
@@ -29,15 +42,23 @@ function OrganizerView() {
     }, [storage]);
 
     useEffect(() => {
-        if(data['profile_image_url']){
-            loadImage(data['profile_image_url']).then((url) => setProfileImage(url))
+        if (data['profile_image_url']) {
+            loadImage(data['profile_image_url']).then((url) => setProfileImage(url));
+        } else {
+            setProfileImage("#");
         }
-        else {
-            setProfileImage("#")
-        }
-    }, [])
+    }, [data, loadImage]);
 
-    // TODO: Implement loading organizer events (bounds to the profile)
+    useEffect(() => {
+        if (data.profile_id && profileEvents === null && !hasFetchedProfileEvents.current) {
+            hasFetchedProfileEvents.current = true;
+            eventAxios.get(`/get/profile?pid=${data.profile_id}`)
+                .then(r => {
+                    setProfileEvents(r.data);
+                })
+                .catch(err => console.log(err));
+        }
+    }, [data.profile_id, profileEvents]);
 
     return (
         profileImage ?
@@ -64,6 +85,24 @@ function OrganizerView() {
                                 <button>Follow</button>
                                 <button>Contact</button>
                             </Stack>
+                            <Stack direction={'row'} textAlign={'center'}>
+                                <Stack style={{borderRight: '1px solid', paddingRight: '1rem', marginRight: '1rem'}}>
+                                    <Typography variant={'h6'} fontWeight={'bold'}>
+                                        {transformNumber(data['total_followers'])}
+                                    </Typography>
+                                    <Typography variant={'subtitle1'} color={'#3a3a3a'} fontSize={'.85rem'}>
+                                        Followers
+                                    </Typography>
+                                </Stack>
+                                <Stack>
+                                    <Typography variant={'h6'} fontWeight={'bold'}>
+                                        {data['total_event_hosted']}
+                                    </Typography>
+                                    <Typography variant={'subtitle1'} color={'#3a3a3a'} fontSize={'.85rem'}>
+                                        Total events
+                                    </Typography>
+                                </Stack>
+                            </Stack>
                             <Stack rowGap={1}>
                                 <p className={'organizer-view-hero__profile-description'}>
                                     {data['description']}
@@ -84,20 +123,42 @@ function OrganizerView() {
                         </Stack>
                     </div>
                 }
-                <div className={'organizer-view-main'}>
+                <Stack className={'organizer-view-main'}>
                     <div style={{borderBottom: '3px solid #c2c2c2', paddingBottom: '.5rem', marginBottom: '2rem'}}>
                         <p style={{borderBottom: '3px solid #ff2d2d', display: 'inline', paddingBottom: '.5rem', fontSize: '1.25rem'}}>
                             Events</p>
                     </div>
                     <p className={'organizer-view-main__title'}>Events</p>
                     <Stack className={'event-category-wrapper'} direction={'row'} columnGap={1}>
-                        <p>Upcoming (0)</p>
-                        <p>Past (0)</p>
+                        <p className={activeTab === 0 ? 'active' : ''} onClick={() => setActiveTab(0)}>Upcoming ({totalUpcoming})</p>
+                        <p className={activeTab === 1 ? 'active' : ''} onClick={() => setActiveTab(1)}>Past ({profileEvents.length - totalUpcoming})</p>
                     </Stack>
                     <div>
-                        <p>Sorry, there are no upcoming events</p>
+                        {profileEvents.length > 0 ?
+                            <Grid container spacing={5} columns={{xs: 16}} style={{marginTop: '1rem'}}>
+                                {
+                                    profileEvents.map((event, index) => {
+                                        const condition = dayjs(event.start_time).isAfter(dayjs());
+                                        if(activeTab === 0){
+                                            return condition &&
+                                                <Grid size={4} key={index}>
+                                                    <EventCard event={event} organizer={data['profile_name']} customURL={data['custom_url']} id={data['profile_id']}/>
+                                                </Grid>
+                                        }
+                                        else{
+                                            return !condition &&
+                                                <Grid size={4} key={index}>
+                                                    <EventCard event={event} organizer={data['profile_name']} customURL={data['custom_url']} id={data['profile_id']}/>
+                                                </Grid>
+                                        }
+                                    })
+                                }
+                            </Grid>
+                            :
+                            <p>Sorry, there are no upcoming events</p>
+                        }
                     </div>
-                </div>
+                </Stack>
             </div>
             :
             <LoadingFallback />
