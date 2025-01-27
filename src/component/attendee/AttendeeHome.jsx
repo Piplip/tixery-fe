ecd4d83db0ec9092046185e4dd1df00ce95ce452
Piplip <93427122+Piplip@users.noreply.github.com>
2025-01-27 20:51:03 +0700

    remove unnecessary code

--- src/component/attendee/AttendeeHome.jsx
+++ src/component/attendee/AttendeeHome.jsx
@@ -1,32 +1,13 @@
-import TopNav from "../shared/TopNav.jsx";
 import AttendeeHero from "./AttendeeHero.jsx";
-import {useEffect, useState} from "react";
-import {clearCookie, getCookie} from "../../common/Utilities.js";
-import {checkLoggedIn} from "../../common/Utilities.js";
 import BrowseEvents from "./BrowseEvents.jsx";
-import RootFooter from "../shared/RootFooter.jsx";
 
 function AttendeeHome(){
-    const [isLoggedIn, setIsLoggedIn] = useState(checkLoggedIn())
-
-    useEffect(() => {
-        if(isLoggedIn === false){
-            if(getCookie('tk') !== null){
-                localStorage.setItem('tk', getCookie('tk'))
-                setIsLoggedIn(true)
-                clearCookie('tk')
-            }
-        }
-    }, [])
-
     // TODO: handle the case where user haven't set up roles
 
     return (
         <div>
-            <TopNav isLoggedIn={isLoggedIn}/>
             <AttendeeHero />
             <BrowseEvents />
-            <RootFooter />
         </div>
     )
 }
